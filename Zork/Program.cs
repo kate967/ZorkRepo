207fa2e55d2a8525f6471361c8803ab251705dee
kate967 <46581081+kate967@users.noreply.github.com>
2020-09-14 23:51:38 -0400

    Zork 1.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Zork
 {
@@ -7,6 +8,22 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+
+            if(inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if(inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
     }
 }

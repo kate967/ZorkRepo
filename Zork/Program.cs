c771c685dda283cd5cc919415f78fc43215e9757
kate967 <46581081+kate967@users.noreply.github.com>
2020-09-14 23:56:39 -0400

    Zork 1.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace Zork
 {
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKOWN
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -10,20 +22,10 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-
-            if(inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if(inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+            Commands command = ToCommand(inputString.Trim());
+            Console.WriteLine(command);
         }
+
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKOWN;
     }
 }

8cef118beab37c0e82a22e1d3515828a8185b5bd
kate967 <46581081+kate967@users.noreply.github.com>
2020-09-14 23:58:07 -0400

    Zork 1.3
    The Game Loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Numerics;
-using System.Runtime.InteropServices;
 
 namespace Zork
 {
@@ -21,11 +20,47 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString = "";
+
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing.";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = "You moved " + command.ToString();
+                        break;
+                    default:
+                        outputString = "Unknown Command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKOWN;
+        private static Commands ToCommand(string commandString)
+        {
+            if (Enum.TryParse<Commands>(commandString, true, out Commands result))
+            {
+                return result;
+            }
+            else
+            {
+                return Commands.UNKOWN;
+            }
+        }
     }
 }

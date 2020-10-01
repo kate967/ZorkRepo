9fa8e1925355934bded453c65d931ad7b8158cb2
kate967 <46581081+kate967@users.noreply.github.com>
2020-10-01 01:31:44 -0400

    +Zork 3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -2,16 +2,32 @@
 using System.Numerics;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using System.Security.Cryptography;
 
 namespace Zork
 {
     class Program
     {
-        private static readonly string[,] Rooms = {
-            {"Rocky Trail", "South of House", "Canyon View"},
-            {"Forest", "West of House", "Behind House"},
-            {"Dense Woods", "North of House", "Clearing" }
+        private static readonly Room[,] Rooms = {
+            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View")},
+            {new Room("Forest"), new Room("West of House"), new Room("Behind House")},
+            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
+
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+            Rooms[1, 1].Description = "This is an open field west of the white house, with a boarded front door.";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large tress all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surroundingh you on the west and south.";
+        }
 
         private static readonly List<Commands> Directions = new List<Commands>
         {
@@ -25,6 +41,8 @@ namespace Zork
 
         static void Main(string[] args)
         {
+            InitializeRoomDescriptions();
+
             Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKOWN;
@@ -40,7 +58,7 @@ namespace Zork
                         Console.WriteLine("Thank you for playing!");
                         break;
                     case Commands.LOOK:
-                        Console.WriteLine ("A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        Console.WriteLine (CurrentRoom.Description);
                         break;
                     case Commands.NORTH:
                     case Commands.SOUTH:
@@ -56,7 +74,7 @@ namespace Zork
             }
         }
 
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -74,16 +92,16 @@ namespace Zork
             bool isValidMove = true;
             switch (command)
             {
-                case Commands.NORTH when playerPos.row < Rooms.GetLength(0) - 1:
-                    playerPos.row++;
-                    break;
-                case Commands.SOUTH when playerPos.row > 0:
+                case Commands.NORTH when playerPos.row <= Rooms.GetLength(0) - 1:
                     playerPos.row--;
                     break;
-                case Commands.EAST when playerPos.col < Rooms.GetLength(1) - 1:
+                case Commands.SOUTH when playerPos.row >= 0:
+                    playerPos.row++;
+                    break;
+                case Commands.EAST when playerPos.col <= Rooms.GetLength(1) - 1:
                     playerPos.col++;
                     break;
-                case Commands.WEST when playerPos.col > 0:
+                case Commands.WEST when playerPos.col >= 0:
                     playerPos.col--;
                     break;
                 default:
---
+++ Zork/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zork
+{
+	public class Room
+	{
+		public string Name { get; }
+
+		public string Description { get; set; }
+
+		public override string ToString() => Name;
+
+        public Room(string name, string description = "")
+        {
+			Name = name;
+			Description = description;
+        }
+	}
+}

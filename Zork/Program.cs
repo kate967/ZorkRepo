1213116f75735dc64b6f1202fe1d7e80b9cc1095
kate967 <46581081+kate967@users.noreply.github.com>
2020-10-07 15:45:33 -0400

    +4.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -3,36 +3,72 @@ using System.Numerics;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using System.Security.Cryptography;
+using System.IO;
+using System.Linq;
 
 namespace Zork
 {
     class Program
     {
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
+        }
+
+        private enum Fields
+        {
+            Name = 0,
+            Description
+        }
+
+        private static readonly Dictionary<string, Room> roomMap;
+
+        static Program()
+        {
+            roomMap = new Dictionary<string, Room>();
+            foreach(Room room in Rooms)
+            {
+                roomMap[room.Name] = room;
+            }
+        }
+   
         private static readonly Room[,] Rooms = {
             {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View")},
             {new Room("Forest"), new Room("West of House"), new Room("Behind House")},
             {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
-        private static void InitializeRoomDescriptions()
+        private static void InitializeRoomDescriptions(string roomFilename)
         {
-            var roomMap = new Dictionary<string, Room>();
-            foreach(Room room in Rooms)
+
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+
+            var roomQuery = from line in File.ReadAllLines(roomFilename)
+                            let fields = line.Split(fieldDelimiter)
+                            where fields.Length == expectedFieldCount
+                            select (Name: fields[(int)Fields.Name],
+                                    Description: fields[(int)Fields.Description]);
+
+            foreach(var (Name, Description) in roomQuery)
             {
-                roomMap[room.Name] = room;
+                roomMap[Name].Description = Description;
             }
 
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+            /*string[] lines = File.ReadAllLines(roomFilename);
+            foreach(string line in lines)
+            {
+                string[] fields = line.Split(fieldDelimiter);
+                if(fields.Length != expectedFieldCount)
+                {
+                    throw new InvalidDataException("Invalid Record");
+                }
 
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of the white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
 
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large tress all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surroundingh you on the west and south.";
+                roomMap[name].Description = description;
+            }*/
         }
 
         private static readonly List<Commands> Directions = new List<Commands>
@@ -48,7 +84,10 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            InitializeRoomDescriptions();
+            
+            const string defaultRoomsFilename = "Rooms.txt";
+            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
+            InitializeRoomDescriptions(roomsFilename);
 
             Room prevRoom = null;
             Commands command = Commands.UNKOWN;

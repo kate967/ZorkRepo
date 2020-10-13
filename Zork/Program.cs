e1a7b3c357b5d90064a397aa8054ea3517733087
kate967 <46581081+kate967@users.noreply.github.com>
2020-10-13 00:06:33 -0400

    +Zork 4.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -13,118 +13,18 @@ namespace Zork
     {
         private enum CommandLineArguments
         {
-            RoomsFilename = 0
+            GameFilename = 0
         }
-
-        private enum Fields
-        {
-            Name = 0,
-            Description
-        }
-
-        private static Room[,] Rooms = {
-            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View")},
-            {new Room("Forest"), new Room("West of House"), new Room("Behind House")},
-            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
-        };
-
-        private static void InitializeRooms(string roomFilename) =>
-            Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomFilename));
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.NORTH,
-            Commands.SOUTH,
-            Commands.EAST,
-            Commands.WEST,
-        };
-        
-        private static (int row, int col) playerPos = (1, 1);
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Zork!");
-            
-            const string defaultRoomsFilename = "Rooms.json";
-            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
-            InitializeRooms(roomsFilename);
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
 
-            Room prevRoom = null;
-            Commands command = Commands.UNKOWN;
-            while (command != Commands.QUIT)
-            {
-                Console.WriteLine(CurrentRoom);
-
-                if (prevRoom != CurrentRoom)
-                {
-                    Console.WriteLine(CurrentRoom.Description);
-                    prevRoom = CurrentRoom;
-                }
-                
-                Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
-
-                switch (command)
-                {
-                    case Commands.QUIT:
-                        Console.WriteLine("Thank you for playing!");
-                        break;
-                    case Commands.LOOK:
-                        Console.WriteLine (CurrentRoom.Description);
-                        break;
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        if (!Move(command))
-                            Console.WriteLine("The way is shut!");
-                        break;
-                    default:
-                        Console.WriteLine("Unknown Command.");
-                        break;
-                }
-            }
+            Game game = Game.Load(gameFilename);
+            Console.WriteLine("Welcome to Zork");
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
         }
-
-        private static Room CurrentRoom
-        {
-            get
-            {
-                return Rooms[playerPos.row, playerPos.col];
-            }
-        }
-
-        private static bool IsDirection(Commands command) =>
-            Directions.Contains(command);
-
-        private static bool Move(Commands command)
-        {
-            Assert.IsTrue(IsDirection(command), "Invalid direction");
-
-            bool isValidMove = true;
-            switch (command)
-            {
-                case Commands.NORTH when playerPos.row > 0:
-                    playerPos.row--;
-                    break;
-                case Commands.SOUTH when playerPos.row < Rooms.GetLength(0) - 1 :
-                    playerPos.row++;
-                    break;
-                case Commands.EAST when playerPos.col < Rooms.GetLength(0) - 1:
-                    playerPos.col++;
-                    break;
-                case Commands.WEST when playerPos.col > 0:
-                    playerPos.col--;
-                    break;
-                default:
-                    isValidMove = false;
-                    break;
-            }
-
-            return isValidMove;
-        }
-
-        private static Commands ToCommand(string commandString) => 
-            Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKOWN;
     }
 }
---
+++ Zork/World.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
+
+namespace Zork
+{
+    public class World
+    {
+        public HashSet<Room> rooms { get; set; }
+
+        [JsonIgnore]
+        public IReadOnlyDictionary<string, Room> RoomsByName => mRoomsByName;
+
+        public Player SpawnPlayer() => new Player(this, StartingLocation);
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            mRoomsByName = rooms.ToDictionary(room => room.Name, room => room);
+
+            foreach(Room room in rooms)
+            {
+                room.UpdateNeighbors(this);
+            }
+        }
+
+        [JsonProperty]
+        private string StartingLocation { get; set; }
+
+        private Dictionary<string, Room> mRoomsByName;
+    }
+}
